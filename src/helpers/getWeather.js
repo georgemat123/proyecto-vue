ab94781d353c161becf720b33395267a4851ed27
georgemat <georgematjtc@gmail.com>
2025-07-09 19:04:49 -0400

    no message

---
+++ src/helpers/getWeather.js
@@ -0,0 +1,10 @@
+import axios  from "axios";
+
+const API_URL = 'https://api.open-meteo.com/v1/forecast?latitude=-16.5&longitude=-68.15&hourly=temperature_2m&timezone=auto'
+
+export const getTemperatura = async () => {
+    const respuesta = await axios.get(API_URL);
+    const nuevaTemperatura =  respuesta.data.hourly.temperature_2m[0];
+    return nuevaTemperatura;
+};
+
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,15 +1,17 @@
 <template>
-<div class="container">
-    <Sidebar></Sidebar>
-    <div class="content">
-        <routerView></routerView>
+    <Header></Header>
+    <div class="container">
+        <Sidebar></Sidebar>
+        <div class="content">
+            <routerView></routerView>
     </div>
 
 </div>
 </template>
 
 <script setup>
-import Sidebar from '../components/Sidebar.vue'
+import Sidebar from '../components/Sidebar.vue';
+import Header from '../components/Header.vue';
 </script>
 
 <style>
